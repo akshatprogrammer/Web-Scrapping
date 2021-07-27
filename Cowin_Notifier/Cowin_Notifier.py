e61b116acf84b2813ed940adccdb632d165ff176
akshatprogrammer <akshat.kodia@gmail.com>
2021-07-27 23:41:17 +0530

    Web Scrapping Part

--- Cowin_Notifier/Cowin_Notifier.py
+++ Cowin_Notifier/Cowin_Notifier.py
@@ -18,3 +18,19 @@ final_dates=[]
 
 for i in all_dates:
     final_dates.append(i.strftime("%d%m%y"))
+    
+# Web Scrapping Logic -> Akshat Jain
+while(True):
+    for d in final_dates:
+        URL = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode={}&date={}".format(pin,d)
+        result = requests.get(URL)
+        json_result = result.json()
+
+        if json_result["centers"]:
+                for center in json_result["centers"]:
+                    for session in center["sessions"]:
+                        print("pincode: "+pin)
+                        print("date: "+d)
+                        print("Center Name: ", center["name"])
+                        print("Center Address: ",center["address"])
+                        print("\n")
